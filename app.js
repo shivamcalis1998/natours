7b2c66e6f18bf8fe7671db799b9d692daec176b2
shivamcalis1998 <shivamswami8899@gmail.com>
2022-09-29 16:18:13 +0530

    Added a comment

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const appError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorControllers");
-const morgan = require("morgan");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
-const cookieParser = require("cookie-parser");
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const appError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorControllers');
+const morgan = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
-const reviewRouter = require("./routes/reviewRoutes");
-const bookingRouter = require("./routes/bookingRoutes");
-const viewRouter = require("./routes/viewRoutes");
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL Middleware
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // Set security http headers
 
 // app.use(
@@ -45,31 +45,31 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: ["'self'", "data:", "blob:"],
+      defaultSrc: ["'self'", 'data:', 'blob:'],
 
       baseUri: ["'self'"],
 
-      fontSrc: ["'self'", "https:", "data:"],
+      fontSrc: ["'self'", 'https:', 'data:'],
 
-      scriptSrc: ["'self'", "https://*.cloudflare.com"],
+      scriptSrc: ["'self'", 'https://*.cloudflare.com'],
 
-      scriptSrc: ["'self'", "https://*.stripe.com"],
+      scriptSrc: ["'self'", 'https://*.stripe.com'],
 
-      scriptSrc: ["'self'", "http:", "https://*.mapbox.com", "data:"],
+      scriptSrc: ["'self'", 'http:', 'https://*.mapbox.com', 'data:'],
 
-      frameSrc: ["'self'", "https://*.stripe.com"],
+      frameSrc: ["'self'", 'https://*.stripe.com'],
 
       objectSrc: ["'none'"],
 
-      styleSrc: ["'self'", "https:", "unsafe-inline"],
+      styleSrc: ["'self'", 'https:', 'unsafe-inline'],
 
-      workerSrc: ["'self'", "data:", "blob:"],
+      workerSrc: ["'self'", 'data:', 'blob:'],
 
-      childSrc: ["'self'", "blob:"],
+      childSrc: ["'self'", 'blob:'],
 
-      imgSrc: ["'self'", "data:", "blob:"],
+      imgSrc: ["'self'", 'data:', 'blob:'],
 
-      connectSrc: ["'self'", "blob:", "https://*.mapbox.com"],
+      connectSrc: ["'self'", 'blob:', 'https://*.mapbox.com'],
 
       upgradeInsecureRequests: [],
     },
@@ -78,19 +78,19 @@ app.use(
 
 // development logging
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 // limit requests form same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: "Too many request from this IP , please Try again in an hour ",
+  message: 'Too many request from this IP , please Try again in an hour ',
 });
-app.use("/api", limiter);
+app.use('/api', limiter);
 //Body paresr,reading data form body into req.body
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 // Data sanitization against NOSQL query injection
 app.use(mongoSanitize());
@@ -102,12 +102,12 @@ app.use(xss());
 app.use(
   hpp({
     whitelist: [
-      "duration",
-      "ratingQuantity",
-      "ratingAverage",
-      "maxGroupSize",
-      "difficulty",
-      "price",
+      'duration',
+      'ratingQuantity',
+      'ratingAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
     ],
   })
 );
@@ -125,16 +125,18 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-app.use("/", viewRouter);
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/review", reviewRouter);
-app.use("/api/v1/bookings", bookingRouter);
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/review', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new appError(`Can't find ${req.originalUrl} on this server !`, 404));
 });
 
 app.use(globalErrorHandler);
 
 module.exports = app;
+
+// start express app
